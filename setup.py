bc4cda2cfda430dd7fd57e4fd0aa7b7dedbb74d6
Pauli Virtanen <pav@iki.fi>
2012-04-01 12:35:34 +0200

    MAINT: Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='GithubSimplePlugin',
-    version='0.1',
+    version='0.1.1',
     author='Pauli Virtanen, Dav Glass',
     author_email='pav@iki.fi',
     description = "Redirects Trac /browser and /changeset urls to github.com",
